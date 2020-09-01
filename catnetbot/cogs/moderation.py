be84c397e546f65ffcbbe2699955f8486b765579
AlekseyZz <53896106+AlekseyZz@users.noreply.github.com>
2020-09-01 14:32:48 +0300

    Обработчик ошибок
    При использовании бота могут возникать различные ошибки, которые будут обрабатываться в этом файле.

--- catnetbot/cogs/moderation.py
+++ catnetbot/cogs/moderation.py
@@ -1,11 +1,20 @@
 import discord
 from discord.ext import commands
 
-
 class Moderation(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(
+        name="hello",  # основное имя d
+        aliases=[],  # вариации ввода команды
+        description="Бот приветствует участника, который вызвал команду",  # описание команды
+        usage="hello"  # пример использование команды
+    )  # декоратор, говорящий боту, что создана команда
+    async def command_hello(self, ctx):  # создание функции с командой
+        await ctx.send(f"Hello, {ctx.author}")
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
+
