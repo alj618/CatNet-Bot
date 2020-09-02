40dd5247bbe29a5cb44b379c1b6c9ba1bf4792d8
X-Aleksey-X <TheHentWill@gmail.com>
2020-09-02 21:26:32 +0300

    Добавив исключение ошибок в случае, если недостаточно прав и исключение ошибки при упущенных обязательных аргументах команды.

--- catnetbot/cogs/errors.py
+++ catnetbot/cogs/errors.py
@@ -1,4 +1,4 @@
-    """Ког с ошибками"""
+"""Ког с ошибками"""
 import discord
 from discord.ext import commands
 import toml_config  # я не помню откуда питон ищет модули, вроде из места запуска
@@ -7,15 +7,16 @@ import toml_config  # я не помню откуда питон ищет мод
 
 conf = toml_config.load_config()["messages"]["errors"]
 
+PREFIX = toml_config.load_config()["bot"]["prefix"]
 
-SUCCESS_LINE = conf["success_line"]["emoji"] * conf["success_line"]["repeat"] + "\n⠀"
-SUCCESS_COLOR = conf["success_color"]["color"]
+SUCCESS_LINE = conf["success_line"]["emoji"] * conf["success_line"]["repeat"] + "\n "
+SUCCESS_COLOR = conf["success_line"]["color"]
 
 STANDART_LINE = conf["standard_line"]["emoji"] * conf["standard_line"]["repeat"] + "\n⠀"
 STANDART_COLOR = conf["standard_line"]["color"]
 
 ERROR_LINE = conf["error_line"]["emoji"] * conf["error_line"]["repeat"] + "\n⠀"
-ERROR_COLOR = conf["error_color"]["color"]
+ERROR_COLOR = conf["error_line"]["color"]
 
 INVISIBLE_SYMBOL = conf["invisible_symbol"]
 
@@ -26,8 +27,6 @@ class Errors(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if ctx.message:
-            await ctx.message.delete()
 
         async def own_command_error_message(problem, solution):
             embed = discord.Embed(color=ERROR_COLOR)
@@ -61,7 +60,18 @@ class Errors(commands.Cog):
                 "Команда с задержкой!",
                 f"Используйте данную команду \nпосле `{make_readable(error.retry_after)}` ⏳",
             )
+        elif isinstance(error, commands.MissingPermissions):
 
+            await own_command_error_message(
+                    "У Вас недостаточно прав!",
+                    "Получите следующие права:\n" + "\n".join(error.missing_perms)
+            )
 
+        elif isinstance(error, commands.MissingRequiredArgument):
+
+            await own_command_error_message(
+                    "Вы упустили аргументы при\nиспользовании команды!",
+                    f"Запишите команду по синтаксису:\n`{PREFIX}{ctx.command.usage}`"
+            )
 def setup(bot):
     bot.add_cog(Errors(bot))
