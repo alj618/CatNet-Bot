108c1a85074bb81a3cd3c71d7f05eb8c883f2f8c
X-Aleksey-X <TheHentWill@gmail.com>
2020-09-03 12:53:49 +0300

    Добавил константы из конфига

--- catnetbot/cogs/moderation.py
+++ catnetbot/cogs/moderation.py
@@ -1,11 +1,24 @@
 import discord
 from discord.ext import commands
+import toml_config
+
+conf = toml_config.load_config()["messages"]["errors"]
+
+SUCCESS_LINE = conf["success_line"]["emoji"] * conf["success_line"]["repeat"] + "\n "
+SUCCESS_COLOR = conf["success_line"]["color"]
+
+STANDART_LINE = conf["standard_line"]["emoji"] * conf["standard_line"]["repeat"] + "\n "
+STANDART_COLOR = conf["standard_line"]["color"]
+
+ERROR_LINE = conf["error_line"]["emoji"] * conf["error_line"]["repeat"] + "\n "
+ERROR_COLOR = conf["error_line"]["color"]
+
+INVISIBLE_SYMBOL = conf["invisible_symbol"]
 
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
