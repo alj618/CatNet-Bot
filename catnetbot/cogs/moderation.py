5417fe4c34e55aab73e7669c49f39798adc32aef
X-Aleksey-X <TheHentWill@gmail.com>
2020-09-03 15:29:41 +0300

    Добавлена команда кика, при помощи которой можно выгонять участников сервера через бота

--- catnetbot/cogs/moderation.py
+++ catnetbot/cogs/moderation.py
@@ -20,5 +20,21 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(
+            name = "кик",
+            aliases = ["kick", "выгнать", "кикнуть"],
+            description = "Выгнать игрока с сервера",
+            usage = "кик [участник] (причина)",
+    )
+    @commands.has_permissions(kick_members = True)
+    async def moderation_command_kick(self, ctx, member: discord.Member, reason = "Не указана"):
+        emb = discord.Embed(color = SUCCESS_COLOR)
+        emb.add_field(name = "Выгнан:", value = f"{member}")
+        emb.add_field(name = "Выгнал:", value = f"{ctx.author.mention}")
+        emb.add_field(name = f"{INVISIBLE_SYMBOL}", value = f"{SUCCESS_LINE}", inline = False)
+        emb.add_field(name = "Причина:", value = f"{reason}")
+        await ctx.send(embed = emb)
+        await member.kick()
+
 def setup(bot):
     bot.add_cog(Moderation(bot))
